f9e92ae6e30cbadb8a9d8c35f18913f3ce55e35a
alundin <alilu684@student.liu.se>
2015-03-11 14:00:58 +0100

    Updating gameworld and gamerender to send car object to render loop

--- core/src/gameworld/GameRenderer.java
+++ core/src/gameworld/GameRenderer.java
@@ -1,18 +1,28 @@
 package gameworld;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 
 /**
  * Created by Alice on 2015-03-11.
  */
 public class GameRenderer {
+    // get a camera object creating a 2D-field
+    private OrthographicCamera cam;
+    private GameWorld myWorld;
 
     //constructor
     public GameRenderer(GameWorld world){
+        myWorld = world;
+        cam = new OrthographicCamera();
+        cam.setToOrtho(true, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
     }
     public void render(){
-       // Gdx.app.log("GameRender", "render");
+        Gdx.app.log("GameRender", "render");
+        Gdx.gl.glClearColor(0, 0, 0, 1); // Creating black background
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
     }
 }
--- core/src/gameworld/GameWorld.java
+++ core/src/gameworld/GameWorld.java
@@ -1,6 +1,7 @@
 package gameworld;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Vector2;
 
 import gameobjects.Car;
 
@@ -10,11 +11,16 @@ import gameobjects.Car;
 public class GameWorld {
     private Car car;
 
-    public GameWorld(){
-
+    public GameWorld() {
+        car = new Car(new Vector2(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()), new Vector2(0,0));
     }
-    public void update(float delta){
-        //Gdx.app.log("GameWorld", "update");
+
+    public void update(float delta) {
+        car.update(0, delta);
+    }
+
+    public Car getCar() {
+        return car;
 
     }
 }
