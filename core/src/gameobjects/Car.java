a57bbfdb21ace5f8c2ce7fd625f6b428cc66af29
novalain <micno751@student.liu.se>
2015-03-11 11:59:07 +0100

    CaBasic functions and variables for car and gameobject

--- core/src/gameobjects/Car.java
+++ core/src/gameobjects/Car.java
@@ -7,10 +7,24 @@ import com.badlogic.gdx.math.Vector2;
 /**
  * Created by Alice on 2015-03-11.
  */
+
 public class Car extends GameObject{
 
     public Car(Vector2 position, Vector2 velocity){
-        super(position, velocity, new Rectangle(position.x, position.y, 10, 10));
+        super(position, velocity, new Rectangle(position.x, position.y, 10, 10)); // Last parameters for width of hitbox
 
     }
+
+    // Update car position based on angle of gyro
+    public void update(int angle) {
+
+        // From previous version
+
+        /*float gx = 90 - angle;
+
+        if(position.x + gx < GameView.window_size.x && position.x + gx > 0)
+            position.x +=Math.round(Math.cos(angle*Math.PI/180)*20);*/
+
+    }
+
 }
--- core/src/gameobjects/GameObject.java
+++ core/src/gameobjects/GameObject.java
@@ -9,14 +9,43 @@ import com.badlogic.gdx.math.Vector2;
 public abstract class GameObject {
     private Vector2 position;
     private Vector2 velocity;
-//    private Vector2 acceleration;
+    private Rectangle hitBox;
+//  private Vector2 acceleration;
+//  private float rotation;
+//  Add Bitmap ?? Spritebatch or whatever it is called in libgdx?
 
-//    private float rotation;
-    private Rectangle box;
-
-    public GameObject(Vector2 position, Vector2 velocity, Rectangle box){
+    public GameObject(Vector2 position, Vector2 velocity, Rectangle hitBox){
         this.position = position;
         this.velocity = velocity;
-        this.box = box;
+        this.hitBox = hitBox;
     }
+
+    // Get functions
+    public Vector2 getPosition(){
+        return position;
+    }
+    public Vector2 getVelocity(){
+        return velocity;
+    }
+    public Rectangle getObjectHitbox(){
+        return hitBox;
+    }
+
+    // Set functions
+    public void setPosition(Vector2 position){
+        this.position = position;
+    }
+    public void setVelocity(Vector2 vel){
+        this.velocity = vel;
+    }
+    public void setObjectHitbow(Rectangle objectHitbox){
+        this.hitBox = objectHitbox;
+    }
+
+    // Draw onto canvas here?
+    public void draw(){
+
+
+    }
+
 }
