19a4857f872f433e53045b37bc291a20be2a791a
novalain <micno751@student.liu.se>
2015-03-18 15:23:02 +0100

    Camera follows physicsbody

--- core/src/main/java/screens/GameScreenWPhysics.java
+++ core/src/main/java/screens/GameScreenWPhysics.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
@@ -29,15 +30,18 @@ public class GameScreenWPhysics implements Screen{
     // Class variables
     private World world;
     private tempCar car;
+    private TireObstacle tire, tire2, tire3, tire4;
     private Box2DDebugRenderer debugRenderer;
     private OrthographicCamera camera;
+
+    private ShapeRenderer shapeRenderer;
 
     private final float TIMESTEP = 1/60f;
     private final int   VELOCITY_ITERATIONS = 8,
                         POSITION_ITERATIONS = 3;
 
     private SpriteBatch batch;
-    private Sprite smileySprite;
+    //private Sprite smileySprite;
 
     private Array<Body> tmpBodies = new Array<Body>();
 
@@ -50,41 +54,57 @@ public class GameScreenWPhysics implements Screen{
         int SCREEN_HEIGHT = Gdx.graphics.getHeight();
         camera = new OrthographicCamera(SCREEN_WIDTH/100,SCREEN_HEIGHT/100);
 
+       // camera.setToOrtho(true, Gdx.graphics.getWidth()/100, Gdx.graphics.getHeight()/100);
+
         batch = new SpriteBatch();
+        batch.setProjectionMatrix(camera.combined);
 
-        car = new tempCar(new Vector2(1.f, -2.0f), 0.5f, world);
+     //   batch.setProjectionMatrix(camera.combined);
 
-        TireObstacle tire = new TireObstacle(new Vector2(0.0f,-1.6f),1.5f,world);
-        TireObstacle tire2= new TireObstacle(new Vector2(0.0f,1.6f),0.5f,world);
-        TireObstacle tire3= new TireObstacle(new Vector2(-1.3f,0.20f),0.5f,world);
-        TireObstacle tire4= new TireObstacle(new Vector2(1.3f,0.20f),0.5f,world);
+        car = new tempCar(new Vector2(0.25f, -2.0f), 0.5f, world);
+
+        tire = new TireObstacle(new Vector2(0.0f,-1.6f),1.5f,world);
+        tire2 = new TireObstacle(new Vector2(0.0f,1.6f),0.5f,world);
+        tire3 = new TireObstacle(new Vector2(-1.3f,0.20f),0.5f,world);
+        tire4 = new TireObstacle(new Vector2(1.3f,0.20f),0.5f,world);
 
     }
 
     @Override
     public void render(float delta) {
-       /* runTime+=delta;
-        // connecting GameWorld and GameRenderer and updating them both
-        world.update(delta);
-        renderer.render(runTime);*/
+
         Gdx.gl.glClearColor(0.7f,0.2f,0.2f,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.setProjectionMatrix(camera.combined);
+
         batch.begin();
         world.getBodies(tmpBodies);
         for (Body body : tmpBodies) {
             //body.applyForceToCenter(12.0f,-11.0f,true);
             if ( body.getUserData() != null && body.getUserData() instanceof Sprite) {
-                Sprite sprite =(Sprite) body.getUserData();
+                Sprite sprite = (Sprite) body.getUserData();
                 sprite.setPosition(body.getPosition().x - sprite.getWidth()/2, body.getPosition().y - sprite.getHeight()/2);
                 sprite.setRotation(body.getAngle() * MathUtils.radiansToDegrees);
                 sprite.draw(batch);
+
             }
         }
+
         batch.end();
-        debugRenderer.render(world, camera.combined);
+
+        handleInput();
 
         world.step(TIMESTEP, VELOCITY_ITERATIONS, POSITION_ITERATIONS);
+
+        camera.position.set(car.getBody().getPosition().x, car.getBody().getPosition().y, 0);
+        camera.update();
+
+        batch.setProjectionMatrix(camera.combined);
+
+        debugRenderer.render(world, camera.combined);
+
+
+
+
     }
 
     @Override
