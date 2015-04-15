ce9d525c72ffcc674b9558346fc9386ef8cab6ab
chren574 <chren574@student.liu.se>
2015-04-15 12:20:33 +0200

    addad a class for the controllermenu

---
+++ ControllerApp/core/src/com/controller_app/ControllerScreen.java
@@ -0,0 +1,43 @@
+package com.controller_app;
+
+import com.badlogic.gdx.Screen;
+
+/**
+ * Created by christoffer on 2015-04-15.
+ */
+public class ControllerScreen implements Screen {
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
