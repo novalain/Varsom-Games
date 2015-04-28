9628cd65207e2b1e34e7b2cb5f39302c4a0e7192
LordMegaDeath3KHaxxor <marli166@student.liu.se>
2015-04-28 09:37:22 +0200

    Simplified the scaled screen.

--- ControllerApp/core/src/com/controller_app/ScaledScreen.java
+++ ControllerApp/core/src/com/controller_app/ScaledScreen.java
@@ -1,41 +1,19 @@
 package com.controller_app;
 
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
 public abstract class ScaledScreen implements Screen {
 
-    protected Main main;
-
-    protected SpriteBatch spriteBatch;
-
     protected Stage stage;
-    protected TextButton.TextButtonStyle textButtonStyle;
-    protected Skin skin;
-    protected TextureAtlas buttonAtlas;
 
     protected OrthographicCamera camera;
     protected Viewport viewport;
 
-    protected BitmapFont font;
-    protected Color backgroundColor;
-
-    protected FreeTypeFontGenerator generator;
-    protected FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
-
     public ScaledScreen() {
-
-        spriteBatch = new SpriteBatch();
 
         camera = new OrthographicCamera();
         viewport = new StretchViewport(Commons.WORLD_WIDTH, Commons.WORLD_HEIGHT, camera);
@@ -44,16 +22,9 @@ public abstract class ScaledScreen implements Screen {
         camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
 
         stage = new Stage();
-        font = new BitmapFont();
 
         stage.getViewport().setCamera(camera);
     }
-
-    abstract void generateFonts();
-
-    abstract void generateUI();
-
-    abstract void generateTextButtonStyle();
 
     protected Stage getStage() {
         return stage;
