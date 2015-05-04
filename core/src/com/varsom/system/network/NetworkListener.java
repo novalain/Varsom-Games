b4d9497d408beea257f92ea8ba19503954473f31
novalain <micno751@student.liu.se>
2015-05-04 15:18:44 +0200

    small changes of variable names and packets

--- core/src/com/varsom/system/network/NetworkListener.java
+++ core/src/com/varsom/system/network/NetworkListener.java
@@ -37,7 +37,7 @@ public class  NetworkListener extends Listener {
         if (o instanceof LoginRequest ) {
             LoginAnswer loginaccepted = new LoginAnswer();
             loginaccepted.accepted = true;
-            c.setName(((LoginRequest) o).playerName);
+            //c.setName(((LoginRequest) o).playerName);
 
             //if the server doesn't permit new clients to join tell the client
             if(!MPServer.joinable) {
@@ -75,7 +75,7 @@ public class  NetworkListener extends Listener {
         }
 
         else if (o instanceof SendDPadData) {
-            int dpad = ((SendDPadData) o).dpaddir;
+            int dpad = ((SendDPadData) o).data;
             System.out.println("Dpad dir is " + dpad);
         }
 
