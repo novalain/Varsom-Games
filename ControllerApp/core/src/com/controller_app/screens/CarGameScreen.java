dc3be04049e5373bd0a2ffe82bf96f0dd6abe7ae
novalain <micno751@student.liu.se>
2015-05-20 17:38:57 +0200

    New krazyRazy-controller design

--- ControllerApp/core/src/com/controller_app/screens/CarGameScreen.java
+++ ControllerApp/core/src/com/controller_app/screens/CarGameScreen.java
@@ -3,10 +3,15 @@ package com.controller_app.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
@@ -23,13 +28,16 @@ import com.controller_app.helper.Commons;
 
 public class CarGameScreen extends ScaledScreen {
 
-    private TextButton buttonDrive;
-    private TextButton buttonReverse;
-    private TextButton buttonHome;
-    private TextButton buttonExit;
-    private TextButton buttonPause;
-    private TextButton btnName;
-    private TextButton buttonPlay;
+    private Image btnAccelerate, btnAcceleratePressed;
+    private Image btnReverse, btnReversePressed;
+    private Image btnPause;
+    private Image btnFire, btnFirePressed;
+    private Image btnAmmo1;
+    private Image btnAmmo2;
+    private Image logo;
+
+    // For current ammo
+    private int ammoSelected = 0;
 
     // Menu stuff
     private Main main;
@@ -42,8 +50,7 @@ public class CarGameScreen extends ScaledScreen {
     private TextButton.TextButtonStyle textButtonStyle;
     private BitmapFont font;
 
-
-    private boolean drivePressed = false, reversePressed = false;
+    private boolean drivePressed = false, reversePressed = false, firePressed = false;
 
     private float steeringSensitivity = 0.4f;
 
@@ -64,7 +71,8 @@ public class CarGameScreen extends ScaledScreen {
         skin = new Skin();
 
         generator = new FreeTypeFontGenerator(Gdx.files.internal("fonts/arial.ttf"));
-        backgroundColor = new Color(0.0f, 0.0f, 7.0f, 1.0f);
+        //backgroundColor = new Color(24.0f/255.f, 102.0f/255.f, 115.0f/255.f, 1.0f);
+        backgroundColor = new Color(1.f, 1.f, 1.f, 1.f);
 
         buttonAtlas = new TextureAtlas(Gdx.files.internal("skins/menuSkin.pack"));
         skin.addRegions(buttonAtlas);
@@ -73,19 +81,24 @@ public class CarGameScreen extends ScaledScreen {
         generateTextButtonStyle();
         generateUI();
 
-        stage.addActor(buttonDrive);
-        stage.addActor(buttonReverse);
-        stage.addActor(buttonHome);
-        stage.addActor(buttonExit);
-        stage.addActor(buttonPause);
-        stage.addActor(btnName);
-        stage.addActor(buttonPlay);
+        stage.addActor(btnAccelerate);
+        stage.addActor(btnAcceleratePressed);
+        stage.addActor(btnReverse);
+        stage.addActor(btnReversePressed);
+        stage.addActor(btnPause);
+        stage.addActor(btnFire);
+        stage.addActor(btnFirePressed);
+        stage.addActor(logo);
+        stage.addActor(btnAmmo1);
+        stage.addActor(btnAmmo2);
 
         mpc.carGameScreen = this;
     }
 
     @Override
     public void show() {
+
+
     }
 
     @Override
@@ -98,6 +111,7 @@ public class CarGameScreen extends ScaledScreen {
 
         printName();
 
+        stage.act();
         stage.draw();
     }
 
@@ -138,50 +152,44 @@ public class CarGameScreen extends ScaledScreen {
     }
 
     private void generateUI() {
-        buttonDrive = new TextButton("Drive", textButtonStyle);
-        buttonDrive.setWidth(1000);
-        buttonDrive.setHeight(900);
-        buttonDrive.setPosition(Commons.WORLD_WIDTH * 0.5f, 0);
 
-        buttonReverse = new TextButton("Reverse", textButtonStyle);
-        buttonReverse.setWidth(1000);
-        buttonReverse.setHeight(180);
-        buttonReverse.setPosition(Commons.WORLD_WIDTH * 0.5f, 900);
+        btnAccelerate = new Image(new Texture(Gdx.files.internal("cargame_controller/krazyRacyAccelerate.png")));
+        btnAccelerate.setPosition(Commons.WORLD_WIDTH - btnAccelerate.getWidth() - 10, Commons.WORLD_HEIGHT - btnAccelerate.getHeight() - 5);
 
-        buttonHome = new TextButton("Menu", textButtonStyle);
-        buttonHome.setWidth(300);
-        buttonHome.setHeight(300);
-        buttonHome.setPosition(Commons.WORLD_WIDTH * 0.2f, Commons.WORLD_HEIGHT * 0.2f);
+        btnAcceleratePressed = new Image(new Texture(Gdx.files.internal("cargame_controller/krazyRacyAcceleratePressed.png")));
+        btnAcceleratePressed.setPosition(Commons.WORLD_WIDTH - btnAccelerate.getWidth() - 10, Commons.WORLD_HEIGHT - btnAccelerate.getHeight() - 5);
+        btnAcceleratePressed.addAction(Actions.sequence(Actions.alpha(0.0f, 0.0f)));
 
-        buttonPause = new TextButton("Pause", textButtonStyle);
-        buttonPause.setWidth(300);
-        buttonPause.setHeight(300);
-        buttonPause.setPosition(Commons.WORLD_WIDTH * 0.2f, Commons.WORLD_HEIGHT * 0.2f + 300f);
+        btnReverse = new Image(new Texture(Gdx.files.internal("cargame_controller/krazyRacyReverse.png")));
+        btnReverse.setPosition(Commons.WORLD_WIDTH - btnReverse.getWidth() - btnAccelerate.getWidth()/4 + 50, 10);
 
-        buttonPlay = new TextButton("Play", textButtonStyle);
-        buttonPlay.setWidth(300);
-        buttonPlay.setHeight(300);
-        buttonPlay.setPosition(Commons.WORLD_WIDTH * 0.2f, Commons.WORLD_HEIGHT * 0.2f + 600f);
+        btnReversePressed = new Image(new Texture(Gdx.files.internal("cargame_controller/krazyRacyReversePressed.png")));
+        btnReversePressed.setPosition(Commons.WORLD_WIDTH - btnReverse.getWidth() - btnAccelerate.getWidth()/4 + 50, 10);
+        btnReversePressed.addAction(Actions.sequence(Actions.alpha(0.0f, 0.0f)));
 
-        buttonExit = new TextButton("Exit", textButtonStyle);
-        buttonExit.setWidth(300);
-        buttonExit.setHeight(300);
-        buttonExit.setPosition(Commons.WORLD_WIDTH * 0.5f - 150f, Commons.WORLD_HEIGHT - 300f);
+        btnPause = new Image(new Texture(Gdx.files.internal("cargame_controller/krazyRacyPause.png")));
+        btnPause.setPosition(Commons.WORLD_WIDTH / 2 - btnPause.getWidth()/2, Commons.WORLD_HEIGHT / 2 - btnPause.getHeight() / 2 );
 
-        btnName = new TextButton(playerName, textButtonStyle);
-        btnName.setWidth(300);
-        btnName.setHeight(300);
-        btnName.setPosition(Commons.WORLD_WIDTH * 0.2f, Commons.WORLD_HEIGHT * 0.2f - 300f);
+        btnFire = new Image(new Texture(Gdx.files.internal("cargame_controller/krazyRacyFire.png")));
+        btnFire.setPosition(50, Commons.WORLD_HEIGHT - btnFire.getHeight() - 50);
 
-        buttonHome.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                Gdx.input.vibrate(main.getVarsomSystemScreen().getVibTime());
-                main.changeScreen(Commons.VARSOM_SYSTEM_SCREEN);
-            }
-        });
+        btnFirePressed = new Image(new Texture(Gdx.files.internal("cargame_controller/krazyRacyFirePressed.png")));
+        btnFirePressed.setPosition(50, Commons.WORLD_HEIGHT - btnFire.getHeight() - 50);
+        btnFirePressed.addAction(Actions.sequence(Actions.alpha(0.0f, 0.0f)));
 
-        buttonPause.addListener(new ClickListener() {
+        logo = new Image(new Texture(Gdx.files.internal("cargame_controller/krazyRacyText.png")));
+        logo.setPosition(Commons.WORLD_WIDTH / 2 - logo.getWidth() / 2, Commons.WORLD_HEIGHT/2 - logo.getHeight() / 2 + 340);
+
+        btnAmmo1 = new Image(new Texture(Gdx.files.internal("cargame_controller/krazyRacyAmmo.png")));
+        btnAmmo2 = new Image(new Texture(Gdx.files.internal("cargame_controller/krazyRacyAmmo.png")));
+
+        btnAmmo1.setPosition(50, 50);
+        btnAmmo2.setPosition(btnAmmo1.getWidth() + 50, 50);
+        btnAmmo1.setOrigin(btnAmmo1.getWidth() / 2, btnAmmo1.getHeight() / 2);
+        btnAmmo2.setOrigin(btnAmmo2.getWidth()/2, btnAmmo2.getHeight()/2);
+        btnAmmo2.setScale(0.5f);
+
+        btnPause.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 Gdx.input.vibrate(main.getVarsomSystemScreen().getVibTime());
@@ -191,58 +199,96 @@ public class CarGameScreen extends ScaledScreen {
             }
         });
 
-        buttonPlay.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                Gdx.input.vibrate(main.getVarsomSystemScreen().getVibTime());
-                //send pause request to server with false
-                mpClient.sendPause(false);
-            }
-        });
-
-        buttonExit.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                Gdx.input.vibrate(main.getVarsomSystemScreen().getVibTime());
-                mpClient.sendExit(true);
-                //Go back to main menu on the server
-
-            }
-        });
-
-
-        buttonDrive.addListener(new ClickListener() {
+        btnAcceleratePressed.addListener(new ClickListener() {
 
             public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
 
                 drivePressed = true;
-
+                btnAcceleratePressed.addAction(Actions.alpha(1.f, 0.f));
                 return true;
             }
 
             public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
                 drivePressed = false;
+                btnAcceleratePressed.addAction(Actions.alpha(0.f, 0.f));
             }
         });
 
-        buttonReverse.addListener(new ClickListener() {
+        btnReversePressed.addListener(new ClickListener() {
 
             public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
 
                 reversePressed = true;
-
-
-
+                btnReversePressed.addAction(Actions.alpha(1.f, 0.f));
                 return true;
             }
 
             public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
                 reversePressed = false;
+                btnReversePressed.addAction(Actions.alpha(0.f, 0.f));
             }
         });
+
+        btnFirePressed.addListener(new ClickListener() {
+
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+
+                System.out.println("BOOOM!!!!");
+                firePressed = true;
+                btnFirePressed.addAction(Actions.alpha(1.f, 0.f));
+                return true;
+            }
+
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                firePressed = false;
+                btnFirePressed.addAction(Actions.alpha(0.f, 0.f));
+            }
+
+        });
+
+        btnAmmo1.addListener(new ClickListener() {
+
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+
+                if(ammoSelected == 1){
+
+                    ammoSelected = 0;
+
+                    // Scale down
+                    btnAmmo2.addAction(Actions.sequence(Actions.scaleTo(0.5f, 0.5f, 0.3f)));
+                    // Scale up
+                    btnAmmo1.addAction(Actions.sequence(Actions.scaleTo(1.0f, 1.0f, 0.3f)));
+
+                }
+
+                return true;
+
+            }
+
+        });
+
+        btnAmmo2.addListener(new ClickListener() {
+
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+
+                if(ammoSelected == 0){
+
+                    ammoSelected = 1;
+
+                    // Scale down
+                    btnAmmo1.addAction(Actions.sequence(Actions.scaleTo(0.5f, 0.5f, 0.3f)));
+                    // Scale up
+                    btnAmmo2.addAction(Actions.sequence(Actions.scaleTo(1.0f, 1.0f, 0.3f)));
+
+                }
+
+                return true;
+
+            }
+
+        });
+
     }
-
-
 
     private void generateTextButtonStyle() {
         textButtonStyle = new TextButton.TextButtonStyle();
@@ -278,6 +324,6 @@ public class CarGameScreen extends ScaledScreen {
     }
 
     public void printName(){
-        btnName.setText(main.getClient().toString());
+        //btnName.setText(main.getClient().toString());
     }
 }
